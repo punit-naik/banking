caf53ca5eab3e45804f7b66bf8ab81360e62f916
Punit Naik <naik.punit44@gmail.com>
2018-09-19 16:32:27 +0530

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject banking "0.1.0"
   :description "Back-end APIs for banking"
-  :url "https://github.com/punit-naik/reminders.git"
+  :url "https://github.com/punit-naik/banking.git"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
